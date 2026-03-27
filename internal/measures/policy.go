51f15cdbaa11a7d526dd43ff89e87f490da9f320
deiv <deiv@localhost.my.domain>
2026-03-26 22:38:25 -0400

    Refatoracao Refatoracao do zero

---
+++ internal/measures/policy.go
@@ -0,0 +1,29 @@
+package measures
+
+type BuiltinPunish interface{
+	Popup()
+	Warn()
+	Notification()
+	ScreenBlocker()
+	MakeItShutdown()
+}
+
+func QuickPopup() {
+
+}
+
+func Warn(){
+
+}
+
+func Notification(){
+
+}
+
+func ScreenBlocker(){
+
+}
+
+func MakeItShutdown(){
+
+}
--- readme.txt
+++
@@ -1,2 +0,0 @@
-/groups não funciona
-/apps/delete
