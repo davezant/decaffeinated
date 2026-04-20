381604f49e8fd61120d0f9792621ad206c327e9c
deiv <deiv@localhost.my.domain>
2026-04-19 21:08:58 -0400

    more functions

--- internal/control/panel.go
+++ internal/control/panel.go
@@ -3,13 +3,25 @@ package control
 import "decaffeinated/internal/watchdog"
 
 type Manager struct {
+	CurrentSessionHash string
 	Watchdog *watchdog.Watchdog
 }
 
-func NewManager(){
+func NewManager(intervalsInSeconds int) Manager{
+	return Manager{
+		Watchdog: watchdog.NewWatchdog(intervalsInSeconds),
+	}
+}
+
+func (m *Manager) IsAdmin(userhash string){
 
 }
 
-func NewGroup(){
+func (m *Manager) LoginParameterstoHash(username string, password string) {
 
+}
+
+func (m *Manager) LoginUser(userhash string){
+	// if userhash right
+	m.CurrentSessionHash = userhash
 }
--- internal/watchdog/watchdog.go
+++ internal/watchdog/watchdog.go
@@ -4,12 +4,13 @@ import (
 	"decaffeinated/internal/processes"
 	"decaffeinated/internal/timers"
 	"log"
+	"slices"
 	"sync"
 	"time"
 )
 
 type Rule struct {
-	GroupName     string
+	RuleName     string
 	AppsNames     []string
 	IsBlocked    bool
 	LimitControl timers.TLimit
@@ -24,6 +25,7 @@ type Watchdog struct {
 }
 
 func NewWatchdog(intervalSeconds int) *Watchdog {
+
 	return &Watchdog{
 		Monitor:         processesmanager.NewMonitor(),
 		RefreshInterval: time.Duration(intervalSeconds) * time.Second,
@@ -32,6 +34,12 @@ func NewWatchdog(intervalSeconds int) *Watchdog {
 }
 
 // Rules 
+
+func NewRule(groupName string) Rule {
+	return Rule{
+		RuleName: groupName,
+	}
+}
 
 func (w *Watchdog) SetRules(rules []Rule){
 	w.rulesMu.Lock()
@@ -51,7 +59,7 @@ func (w *Watchdog) RemoveRule(name string){
 	var newRules []Rule
 
 	for _, c := range w.Rules {
-		if c.GroupName != name {
+		if c.RuleName != name {
 			newRules = append(newRules, c)
 		} else {
 			continue
@@ -59,34 +67,87 @@ func (w *Watchdog) RemoveRule(name string){
 	}
 	w.Rules = newRules
 }
+// --- Gestão de Regras ---
 
-func (w *Watchdog) BlockRule() {
-
+func (w *Watchdog) BlockRule(name string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == name {
+			w.Rules[i].IsBlocked = true
+		}
+	}
 }
 
-func (w *Watchdog) UnblockRule() {
-
+func (w *Watchdog) UnblockRule(name string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == name {
+			w.Rules[i].IsBlocked = false
+		}
+	}
 }
 
-func (w *Watchdog) RenameRule() {
-
+func (w *Watchdog) RenameRule(oldName string, newName string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == oldName {
+			w.Rules[i].RuleName = newName
+		}
+	}
 }
 
-func (w *Watchdog) ActivateRule() {
-
+func (w *Watchdog) ActivateRule(name string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == name {
+			w.Rules[i].active = true
+			w.Rules[i].LimitControl.Toggle(true)
+		}
+	}
 }
 
-func (w *Watchdog) DeactivateRule() {
-
+func (w *Watchdog) DeactivateRule(name string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == name {
+			w.Rules[i].active = false
+			w.Rules[i].LimitControl.Toggle(false)
+		}
+	}
 }
-// Apps 
 
-func (w *Watchdog) AddAppToRule() {
-
+func (w *Watchdog) AddAppToRule(ruleName string, appName string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == ruleName {
+			if slices.Contains(w.Rules[i].AppsNames, appName){
+				return
+			}
+			w.Rules[i].AppsNames = append(w.Rules[i].AppsNames, appName)
+		}
+	}
 }
 
-func (w *Watchdog) RemoveAppFromRule() {
-
+func (w *Watchdog) RemoveAppFromRule(ruleName string, appName string) {
+	w.rulesMu.Lock()
+	defer w.rulesMu.Unlock()
+	for i := range w.Rules {
+		if w.Rules[i].RuleName == ruleName {
+			var updatedApps []string
+			for _, name := range w.Rules[i].AppsNames {
+				if name != appName {
+					updatedApps = append(updatedApps, name)
+				}
+			}
+			w.Rules[i].AppsNames = updatedApps
+		}
+	}
 }
 
 func (w *Watchdog) Start(verbose bool) {
@@ -96,7 +157,7 @@ func (w *Watchdog) Start(verbose bool) {
 
 	for range ticker.C {
 		w.Monitor.RefreshCurrentProcesses()
-	
+
 		w.rulesMu.RLock()
 		for i := range w.Rules {
 			rule := &w.Rules[i]
@@ -114,15 +175,15 @@ func (w *Watchdog) Start(verbose bool) {
 				}
 				continue
 			}
-
+			
 			if isRunning && !rule.active {
 				rule.LimitControl.Toggle(true)
 				rule.active = true
-				if verbose { log.Printf("[Watchdog] Rule %s activated", rule.GroupName) }
+				if verbose { log.Printf("[Watchdog] Rule %s activated", rule.RuleName) }
 			} else if !isRunning && rule.active {
 				rule.LimitControl.Toggle(false)
 				rule.active = false
-				if verbose { log.Printf("[Watchdog] Rule %s deactivated", rule.GroupName) }
+				if verbose { log.Printf("[Watchdog] Rule %s deactivated", rule.RuleName) }
 			}
 		}
 		w.rulesMu.RUnlock()
